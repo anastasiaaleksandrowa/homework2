2f4ff5dd9b0fa5470148b70f9a72633c300a204c
Kira <kirnan_t@outlok.com>
2024-03-30 15:21:39 +0700

    add task1 %%7and23

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+int num = new Random().Next(0, 10000000);
+Console.WriteLine(num);
+
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
